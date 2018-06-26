c5df639c2b9d5406ae1b7ac2d490be566b8bbe9f
Manzo <Matthew.Manzo@us.nationalgrid.com>
2018-06-26 15:40:57 -0400

    first version

---
+++ bookdetail.js
@@ -0,0 +1,16 @@
+function versionchange(value){
+	if(value==1){
+		document.getElementById("box1").style.display="block";
+		document.getElementById("box2").style.display="none";
+	}
+	if(value==2){
+		document.getElementById("box1").style.display="none";
+		document.getElementById("box2").style.display="block";
+	}
+}
+
+
+function addtowish()
+{
+	alert("Add to wish list!");
+}
